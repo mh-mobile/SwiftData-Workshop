7a253256f133cbd90bdadcfd1388e01a7633a188
mh-mobile <mh.mobiler@gmail.com>
2024-03-26 14:58:45 +0900

    データの作成

--- Gustro/Gustro/ContentView.swift
+++ Gustro/Gustro/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var modelContext
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                VStack {
+                    Text("hoge")
+                }
+            }
+            .navigationTitle("レストラン")
+            .toolbar {
+                ToolbarItem {
+                    Button("add", systemImage: "plus", action: addItems)
+                }
+            }
         }
-        .padding()
+    }
+    
+    func addItems() {
+        let names = [
+            "Wok this Way",
+            "Thyme Square",
+            "Pasta la Vista",
+            "Life of Pie",
+            "Lord of the Wings"
+        ]
+        
+        for name in names {
+            let item = Restaurant(name: name, priceRating: 2, qualityRating: 3, speedRating: 4)
+            modelContext.insert(item)
+        }
     }
 }
 
